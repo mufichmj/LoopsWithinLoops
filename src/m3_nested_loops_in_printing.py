9315b7be37c6eb6a28bda91771850c8cec78589e
mufichmj <mufichmj@rose-hulman.edu>
2018-04-25 21:07:03 -0400

    m3

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -67,8 +67,6 @@ def rectangle_of_stars(r, c):
         print()
 
 
-
-
 def run_test_triangle_of_stars():
     """ Tests the    triangle_of_stars    function. """
     print()
@@ -104,7 +102,7 @@ def triangle_of_stars(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     #  *** Unless your instructor directs you otherwise,
@@ -122,7 +120,10 @@ def triangle_of_stars(r):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
-
+    for k in range(r):
+        for j in range(k + 1):
+            print('*', end='')
+        print()
 
 def run_test_decreasing_exclamation_marks():
     """ Tests the    decreasing_exclamation_marks    function. """
